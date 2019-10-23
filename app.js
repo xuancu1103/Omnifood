8c667d94d62ea2fd8d49bb4cd7db9f99048554bd
DalatCoder <hieuntctk42@gmail.com>
2019-10-23 18:29:54 +0700

    Create home route

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
